5604607f03260a9a3002bdc12c0d42917203e816
Pol de la Hoz <49729957+poldelahoz@users.noreply.github.com>
2023-02-02 18:32:32 +0100

    T02N01 exercisi1

--- T02N01/lib/src/main/java/exercisi1/AgentDeBorsa.java
+++ T02N01/lib/src/main/java/exercisi1/AgentDeBorsa.java
@@ -3,7 +3,7 @@ package exercisi1;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AgentDeBorsa implements IAgentDeBorsa{
+public class AgentDeBorsa{
 	
 	private String name;	
 	private List<AgenciaDeBorsa> subscribers;
@@ -13,26 +13,26 @@ public class AgentDeBorsa implements IAgentDeBorsa{
 		this.subscribers = new ArrayList<AgenciaDeBorsa>();
 	}
 	
-	@Override
-	public void subscribe(AgenciaDeBorsa agenciaDeBorsa) {
-		subscribers.add(agenciaDeBorsa);
-	}
-
-	@Override
-	public void unsubscribe(AgenciaDeBorsa agenciaDeBorsa) {
-		subscribers.remove(agenciaDeBorsa);
-	}
-	
-	@Override
-	public void notifySubs(String notification) {
-		subscribers.forEach(c -> c.update(this.name, notification));
-	}
-
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	//@Override
+	public void subscribe(AgenciaDeBorsa agenciaDeBorsa) {
+		subscribers.add(agenciaDeBorsa);
+	}
+
+	//@Override
+	public void unsubscribe(AgenciaDeBorsa agenciaDeBorsa) {
+		subscribers.remove(agenciaDeBorsa);
+	}
+	
+	//@Override
+	public void notifySubs(String notification) {
+		subscribers.forEach(c -> c.update(this.name, notification));
 	}
 }
